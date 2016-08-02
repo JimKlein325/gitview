0a6ceb21e23c6f138c820cd8df75e1a6c00049b7
Jim Klein <j.mesklein@gmail.com>
2016-08-02 14:09:11 -0700

    clean up files, removing extraneous space an outdated comments.

--- js/getview-interface.js
+++ js/getview-interface.js
@@ -1,18 +1,15 @@
 var apiKey = require('./../.env').apiKey;
-
 var RepoList = require('./../js/view.js').RepoList;
-
 var RepoItem = require('./../js/view.js').RepoItem;
 
 var displayRepos = function(repoItems, errorMessage) {
-
   if (errorMessage === "success") {
     for(var i = 0; i < repoItems.length ; i++) {
       var name = repoItems[i].name;
       var description = repoItems[i].description;
       var repoItem = repoItems[i];
       $('.repoContents').append(repoItem.writeHtml());
-    };
+    }
   }
   else {
     $('.repoContents').append("<p>" + errorMessage + "</p>");
@@ -21,11 +18,10 @@ var displayRepos = function(repoItems, errorMessage) {
 
 $(document).ready(function() {
   $('#viewRepoContents').click(function() {
-    var currentList = new RepoList();
-    //currentWeatherObject.getWeather();
     var userName = $('#username').val();
     $('#username').val("");
-    $('.repoContents').text("Test.");
+    $('.repoContents').text("");
+    var currentList = new RepoList();
     currentList.getRepos(userName, displayRepos);
   });
 });
--- js/view.js
+++ js/view.js
@@ -1,50 +1,36 @@
   var apiKey = require('./../.env').apiKey;
-
-  // exports.RepoItem = function RepoItem(name, description) {
-  //   this.name = name;
-  //   this.description = description;
-  // };
-
-
   function RepoItem (name, description) {
     this.name = name;
-    this.description = description;
+    if(description === null){
+      this.description = "No description given.";
+    }
+    else if (description ==="") {
+      this.description = "No description given.";
+    }
+    else {
+      this.description = description;
+    }
   };
-
   RepoItem.prototype.writeHtml = function() {
     return "<h2>" + this.name + "</h2>" + "<p>" + this.description + "</p>";
   };
 
   exports.RepoList = function(){
   };
-
   exports.RepoList.prototype.getRepos = function(userName, displayFunction){
-    //get data
-    var uName = "JimKlein325o";
-
-var items = [];
-
-    // TODO:  GET json request and figure out how to get the json array.
-
-    $.get('https://api.github.com/users/' + uName + '/repos?access_token=' + apiKey).then(function(response){
-
+    var items = [];
+    $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey)
+    .then(function(response){
         var responseArray = response;
-        //console.log(repoArray);
-
         for(var i = 0; i < responseArray.length ; i++) {
           var repo = responseArray[i];
           var name = repo.name;
-
           var repoItem = new RepoItem(repo.name, repo.description);
           items.push(repoItem);
-          //debugger;
         }
         displayFunction(items, "success");
-        //debugger;
-
-    }).fail(function(error){
-//alert("boo!");
-    // console.log(error.responseJSON.message);
+    })
+    .fail(function(error){
     var message = "Error: " + error.responseJSON.message + ". Please check that the user name is correct.";
     displayFunction(items, message);
   });
